5c0c3f6c72bc3ba3c3231305637e6dce0d536661
iamyahia <yahiabaiz@gmail.com>
2023-09-21 00:35:30 +0300

    feat: remove background color and change text color and add border to the body

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -10,7 +10,6 @@ import {
   Typography,
   Link,
 } from "@mui/material";
-// import Link from "next/link";
 import React from "react";
 
 const pages = [
@@ -36,29 +35,50 @@ const StyledToolbar = styled(Toolbar)({
   display: "flex",
   justifyContent: "space-between",
 });
+
+const StyledLink = styled(Link)({
+  textDecoration: "none",
+  textTransform: "lowercase",
+  fontWeight: "normal",
+  listStyle: "none",
+});
 export const Navbar = () => {
   return (
-    <AppBar position="static" style={{ display: "flex" }}>
+    <AppBar
+      position="static"
+      style={{
+        display: "flex",
+        borderBottom: "1px solid #1E2D3D",
+        boxShadow: "none",
+      }}
+      color="transparent"
+    >
       <StyledToolbar>
-        <Typography variant="p" component="p">
+        <Typography
+          variant="p"
+          component="p"
+          style={{ marginRight: "9.75rem" }}
+        >
           yahia_hasan
         </Typography>
         <Box sx={{ display: { xs: "none", md: "flex", width: "100%" } }}>
           {pages.map((page, index) => (
-            <Button
+            <StyledLink
               key={page.name}
-              onClick={() => {
-                return alert("clicked");
-              }}
+              href={page.href}
+              color="inherit"
+              // variant="li"
+              // component="li"
               sx={{ my: 2, color: "white" }}
               style={{
+                padding: "0 32px",
                 marginLeft: pages.length - 1 === index ? "auto" : "",
+                // borderRight: "1px double #ff6550",
+                borderLeft: "1px double #1e2d3d",
               }}
             >
-              <Link href={page.href} color="inherit">
-                {page.name}
-              </Link>
-            </Button>
+              {page.name}
+            </StyledLink>
           ))}
         </Box>
       </StyledToolbar>
