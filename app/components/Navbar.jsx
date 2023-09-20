1d1b1c1eb9329c9d4639d6c9b2bbd93a64e6cb41
iamyahia <yahiabaiz@gmail.com>
2023-09-20 23:41:31 +0300

    feat: implement navbar structure

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -1,5 +1,67 @@
+"use client";
+import styled from "@emotion/styled";
+import {
+  AppBar,
+  Box,
+  Button,
+  Menu,
+  MenuItem,
+  Toolbar,
+  Typography,
+  Link,
+} from "@mui/material";
+// import Link from "next/link";
 import React from "react";
 
+const pages = [
+  {
+    name: "_hello",
+    href: "/",
+  },
+  {
+    name: "_about-me",
+    href: "/about-me",
+  },
+  {
+    name: "_projects",
+    href: "/projects",
+  },
+  {
+    name: "_contact-me",
+    href: "/contact-me",
+  },
+];
+
+const StyledToolbar = styled(Toolbar)({
+  display: "flex",
+  justifyContent: "space-between",
+});
 export const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <AppBar position="static" style={{ display: "flex" }}>
+      <StyledToolbar>
+        <Typography variant="p" component="p">
+          yahia_hasan
+        </Typography>
+        <Box sx={{ display: { xs: "none", md: "flex", width: "100%" } }}>
+          {pages.map((page, index) => (
+            <Button
+              key={page.name}
+              onClick={() => {
+                return alert("clicked");
+              }}
+              sx={{ my: 2, color: "white" }}
+              style={{
+                marginLeft: pages.length - 1 === index ? "auto" : "",
+              }}
+            >
+              <Link href={page.href} color="inherit">
+                {page.name}
+              </Link>
+            </Button>
+          ))}
+        </Box>
+      </StyledToolbar>
+    </AppBar>
+  );
 };
