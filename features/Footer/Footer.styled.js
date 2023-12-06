564db1df3eb96de0789df8613bfe349aa3bb38ff
iamyahia <yahiabaiz@gmail.com>
2023-12-06 11:43:39 +0300

    remove tailwind

---
+++ features/Footer/Footer.styled.js
@@ -0,0 +1,15 @@
+"use client";
+
+import { styled } from "@mui/material";
+
+const Container = styled("footer")({
+  width: "100%",
+  display: "flex",
+  borderTop: "1px solid #1E2D3D",
+});
+
+const ItemTest = styled("p")({
+  backgroundColor: "red",
+});
+
+export { Container, ItemTest };
