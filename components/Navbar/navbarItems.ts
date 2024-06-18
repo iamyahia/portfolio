7b3b29999dad7f3010c815b405fd0e0a815cff91
iamyahia <yahiabaiz@gmail.com>
2024-06-18 15:57:16 +0300

    refactor: Update Navbar component to use consistent navigation links and fix padding styling

--- components/Navbar/navbarItems.ts
+++ components/Navbar/navbarItems.ts
@@ -1,6 +1,6 @@
 export const navItems = [
   { key: "home", value: "_hello", href: "/" },
   { key: "articles", value: "_about-me", href: "/about-me" },
-  { key: "projects", value: "_projects", href: "/" },
-  { key: "about", value: "_articles", href: "/" },
+  { key: "projects", value: "_projects", href: "/projects" },
+  { key: "about", value: "_articles", href: "/articles" },
 ];
